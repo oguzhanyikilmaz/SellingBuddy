f675454831cccdbbddc8297f08ad41d78cfd7b96
Oğuzhan <49417199+oguzhanyikilmaz@users.noreply.github.com>
2022-09-18 14:26:17 +0300

    RabbitMQ and Factory Completed.

---
+++ src/BuildingBlocks/EventBus/EventBus.Factory/EventBusFactory.cs
@@ -0,0 +1,24 @@
+﻿using EventBus.Base;
+using EventBus.Base.Abstraction;
+using EventBus.RabbitMQ;
+using EventBus.AzureServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Factory
+{
+  public  class EventBusFactory
+    {
+        public static IEventBus Create(EventBusConfig config,IServiceProvider serviceProvider)
+        {
+            return config.EventBusType switch
+            {
+                EventBusType.AzureServiceBus => new EventBusServiceBus(serviceProvider,config),
+                _ => new EventBusRabbitMQ(serviceProvider,config),
+            };
+        }
+    }
+}
---
+++ src/BuildingBlocks/EventBus/EventBus.RabbitMQ/RabbitMQPersistentConnection.cs
@@ -0,0 +1,86 @@
+﻿using Polly;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.RabbitMQ
+{
+    public class RabbitMQPersistentConnection : IDisposable
+    {
+        private IConnection connection;
+        private readonly IConnectionFactory connectionFactory;
+        private readonly int retryCount;
+        private object lock_object = new object();
+        private bool _disposed;
+
+        public bool IsConnection => connection != null && connection.IsOpen;
+        public RabbitMQPersistentConnection(IConnectionFactory connectionFactory, int retryCount = 5)
+        {
+            this.connectionFactory = connectionFactory;
+            this.retryCount = retryCount;
+        }
+
+        public IModel CreateModel()
+        {
+            return connection.CreateModel();
+        }
+        public void Dispose()
+        {
+            connection.Dispose();
+            _disposed = true;
+        }
+
+        public bool TryConnect()
+        {
+            lock (lock_object)
+            {
+                var policy = Policy.Handle<SocketException>()
+                    .Or<BrokerUnreachableException>()
+                    .WaitAndRetry(retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (ex, time) =>
+                    { }
+                );
+
+                policy.Execute(() =>
+                {
+                    connection = connectionFactory.CreateConnection();
+                });
+
+                if (IsConnection)
+                {
+                    connection.ConnectionShutdown += Connection_ConnectionShutdown;
+                    connection.CallbackException += Connection_CallbackException; ;
+                    connection.ConnectionBlocked += Connection_ConnectionBlocked;
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        private void Connection_ConnectionBlocked(object sender, global::RabbitMQ.Client.Events.ConnectionBlockedEventArgs e)
+        {
+            if (_disposed) return;
+            //log
+            TryConnect();
+        }
+
+        private void Connection_CallbackException(object sender, global::RabbitMQ.Client.Events.CallbackExceptionEventArgs e)
+        {
+            if (_disposed) return;
+            //log
+            TryConnect();
+        }
+
+        private void Connection_ConnectionShutdown(object sender, ShutdownEventArgs e)
+        {
+            if (_disposed) return;
+            //log
+            TryConnect();
+        }
+    }
+}
