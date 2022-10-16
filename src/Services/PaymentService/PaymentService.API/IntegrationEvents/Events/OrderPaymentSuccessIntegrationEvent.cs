7138ae1bb2269383853d614329c08c00d338d07e
Oğuzhan <49417199+oguzhanyikilmaz@users.noreply.github.com>
2022-10-17 00:39:45 +0300

    PaymentService_Modified.

---
+++ src/Services/PaymentService/PaymentService.API/IntegrationEvents/Events/OrderPaymentSuccessIntegrationEvent.cs
@@ -0,0 +1,14 @@
+﻿using EventBus.Base.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentService.API.IntegrationEvents.Events
+{
+    public class OrderPaymentSuccessIntegrationEvent : IntegrationEvent
+    {
+        public int OrderId { get; }
+        public OrderPaymentSuccessIntegrationEvent(int orderId) => OrderId = orderId;
+    }
+}
