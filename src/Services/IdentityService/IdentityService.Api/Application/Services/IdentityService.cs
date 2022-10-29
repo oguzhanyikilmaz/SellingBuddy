0aa15b699eab24ec49ebae03260b927ad47fce07
Oğuzhan <49417199+oguzhanyikilmaz@users.noreply.github.com>
2022-10-29 23:46:27 +0300

    IdentityService_Completed.

---
+++ src/Services/IdentityService/IdentityService.Api/Application/Services/IdentityService.cs
@@ -0,0 +1,40 @@
+﻿using IdentityService.Api.Application.Models;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityService.Api.Application.Services
+{
+    public class IdentityService : IIdentityService
+    {
+        public Task<LoginResponseModel> Login(LoginRequestModel requestModel)
+        {
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier,requestModel.UserName),
+                new Claim(ClaimTypes.Name,"Oğuzhan Yıkılmaz"),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("OguzhanYklmzSecretKeyShouldBeLong"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expiry = DateTime.Now.AddDays(10);
+
+            var token = new JwtSecurityToken(claims: claims, expires: expiry, signingCredentials: creds, notBefore: DateTime.Now);
+
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            LoginResponseModel response = new LoginResponseModel()
+            {
+                UserName = requestModel.UserName,
+                UserToken = encodedJwt
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
